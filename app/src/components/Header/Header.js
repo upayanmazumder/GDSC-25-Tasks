eaeb34a4cecdd5f25671058a64218d25e5071bba
Upayan Mazumder <upayanm3@gmail.com>
2025-03-05 20:57:38 +0530

    Refactor Header and Footer components for improved styling and layout; update CSS variables for theme consistency

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -9,16 +9,19 @@ const Header = () => {
             <nav>
                 <ul>
                     <li><a href="/">Home</a></li>
-                    {user ? (
-                        <>
-                            <li>Welcome, {user.username}</li>
-                            <li><button onClick={logout}>Logout</button></li>
-                        </>
-                    ) : (
-                        <li><a href="/login">Login</a></li>
-                    )}
+                    <li><a href="https://github.com/upayanmazumder/GDSC-25-Tasks">Repository</a></li>
                 </ul>
             </nav>
+            <div className={headerStyles.auth}>
+                {user ? (
+                    <ul>
+                        <li>Welcome, {user.username}</li>
+                        <li><button onClick={logout}>Logout</button></li>
+                    </ul>
+                ) : (
+                    <li><a href="/login">Login</a></li>
+                )}
+            </div>
         </header>
     );
 }
