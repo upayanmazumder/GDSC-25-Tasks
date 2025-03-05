2555390d3c0707c88ef96ab375efd8ecf64b1bbc
Upayan Mazumder <upayanm3@gmail.com>
2025-03-05 22:14:52 +0530

    Refactor CSS styles for Login, Header, PostFeed, Profile components; improve layout and animations

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,21 +1,11 @@
-import { useAuth } from "../Context/AuthContext";
-import { useNavigate } from "react-router-dom";
 import PostFeed from "../components/PostFeed/PostFeed"
 
 const Home = () => {
-  const { logout } = useAuth();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logout();
-    navigate("/login");
-  };
-
-  return (
-    <main>
-      <PostFeed />
-    </main>
-  );
+    return (
+        <main>
+            <PostFeed />
+        </main>
+    );
 };
 
 export default Home;
--- app/src/pages/PostContent.js
+++ app/src/pages/PostContent.js
@@ -1,11 +1,11 @@
 import PostDetails from "../components/PostDetails/PostDetails";
 
 const PostContent = () => {
-  return (
-    <main>
-      <PostDetails />
-    </main>
-  );
+    return (
+        <main>
+            <PostDetails />
+        </main>
+    );
 }
 
 export default PostContent;
