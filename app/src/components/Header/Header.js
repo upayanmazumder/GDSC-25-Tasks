4b3d52d878cb137dc5fd9dad4f5286dbd597bf9d
Upayan Mazumder <upayanm3@gmail.com>
2025-03-05 19:58:04 +0530

    Add navigation links to Header component

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -3,7 +3,12 @@ import headerStyles from "./Header.module.css";
 const Header = () => {
     return (
         <header className={headerStyles.header}>
-            <h1>Upayan's GDSC Recruitments Submission</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/login">Login</a></li>
+                </ul>
+            </nav>
         </header>
     );
 }
